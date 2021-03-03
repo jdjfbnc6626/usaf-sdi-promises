e0f01cb3005211cfb04d3459cede3ce26c9f742b
jjtu WSL2 box <gmail>
2021-03-03 13:37:46 -0500

    Solved first file and create dev-working branch

--- exercises/bare_minimum/callbackReview.js
+++ exercises/bare_minimum/callbackReview.js
@@ -6,13 +6,30 @@ var fs = require('fs');
 var request = require('request');
 
 // This function should retrieve the first line of the file at `filePath`
-var pluckFirstLineFromFile = function (filePath) {
-  // TODO
+var pluckFirstLineFromFile = function (filePath, nextInstructions) { //ADDED NEXT INSTRUCTIONS WITH ZACH
+  //TODO
+  fs.readFile(filePath, 'utf8', function(err, content) {
+    if (err) {
+      nextInstructions(err);
+    } else {
+      const firstLine = content.split('\n');
+      nextInstructions(null, firstLine[0]);
+    }
+  }); //TODO - PASSING SOLUTION
 };
 
+
 // This function should retrieve the status code of a GET request to `url`
-var getStatusCode = function (url) {
-  // TODO
+var getStatusCode = function (url, nextStep) {
+  //TODO
+  request(url, function (error, response, body) {
+    if (error) {
+      nextStep(error);
+    } else {
+      var statusCode = response.statusCode;
+      nextStep(undefined, statusCode);
+    }
+  }) //TODO - completed to show 6 passing
 };
 
 // Export these functions so we can test them and reuse them in later exercises
--- exercises/bare_minimum/promiseConstructor.js
+++ exercises/bare_minimum/promiseConstructor.js
@@ -7,15 +7,40 @@
 var fs = require('fs');
 var request = require('request');
 var Promise = require('bluebird');
+const { resolve } = require('bluebird');
 
 // This function should retrieve the first line of the file at `filePath`
 var pluckFirstLineFromFileAsync = function(filePath) {
   // TODO
+  var pluckPromise = new Promise((resolve, reject) => {
+    fs.readFile(filePath, 'utf8', function(err, contents){
+      if(err){
+        reject(err);
+      } else {
+        var firstLine = contents.split('\n');
+        resolve(firstLine[0]);
+      }
+    })
+  });
+  return pluckPromise;
+  //TODO
 };
 
 // This function should retrieve the status code of a GET request to `url`
 var getStatusCodeAsync = function(url) {
   // TODO
+  var getStatusPromise = new Promise((resolve, reject) => {
+    request(url, function (error, response, body) {
+      if (error) {
+        reject(error);
+      } else {
+        var statusCode = response.statusCode;
+        resolve(statusCode);
+      }
+    });
+  });
+  return getStatusPromise;
+  //TODO
 };
 
 // Export these functions so we can test them and reuse them in later exercises
